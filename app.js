c5ed95e50ec7aa4ff1bbec2c569ba7c8042d0023
Lucas-Vergara <lucasvergara1@gmail.com>
2021-02-27 15:53:18 -0300

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var app = express();
 app.use(helmet());
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://Luc:fwbUiGQAQ8lnEKyf@cluster0.gf1as.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://Luc:fwbUiGQAQ8lnEKyf@cluster0.gf1as.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
